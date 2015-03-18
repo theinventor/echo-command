0a0933bfde10b75db36a2a406210e50774637873
Troy Anderson <the.inventor@gmail.com>
2015-03-18 12:20:59 -0700

    add a response

--- app/controllers/commands_controller.rb
+++ app/controllers/commands_controller.rb
@@ -8,6 +8,22 @@ class CommandsController < ApplicationController
 
   def create
     Rails.logger.debug "CREATE_PARAMS: #{params}"
+
+    if params[:session] && params[:session][:new]
+      response = {
+          version: '1',
+          response: {
+              outputSpeech: {
+                  type: "PlainText", text: "Hi Troy, you opened the app!"
+              },
+              shouldEndSession: true
+          }
+      }
+      render json: response and return
+
+    end
+
+
     render text: "OK"
   end
 
---
+++ app/views/commands/create.json.jbuilder
@@ -0,0 +1,19 @@
+{
+    "version" : "string",
+    "sessionAttributes" : {
+    "string" : object
+},
+    "response" : {
+    "outputSpeech" : {
+    "type" : "string",
+    "text" : "string"
+},
+    "card" : {
+    "type" : "string",
+    "title" : "string",
+    "subtitle" : "string",
+    "content" : "string"
+},
+    "shouldEndSession" : boolean
+}
+}
