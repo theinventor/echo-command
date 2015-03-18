89101a70e47faec9fd95dfc990f7fb76b23f9e2c
Troy Anderson <the.inventor@gmail.com>
2015-03-18 11:41:58 -0700

    update debug statements

--- app/controllers/commands_controller.rb
+++ app/controllers/commands_controller.rb
@@ -1,7 +1,12 @@
 class CommandsController < ApplicationController
 
   def index
-    puts "PARAMS: #{params}"
+    puts "INDEX_PARAMS: #{params}"
+    render text: "OK"
+  end
+
+  def create
+    puts "CREATE_PARAMS: #{params}"
     render text: "OK"
   end
 
